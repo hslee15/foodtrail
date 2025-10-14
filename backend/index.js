835a5fb76de2d130984f8580e6f2afd6ce317c14
hslee15 <lhs01019@kbu.ac.kr>
2025-10-14 21:06:06 +0900

    ii

--- backend/index.js
+++ backend/index.js
@@ -8,25 +8,28 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT
 
-
 app.use(cors({
   origin: process.env.FRONT_ORIGIN,
   credentials: true
 }));
 app.use(express.json({ limit: "2mb" }));
 app.use(cookieParser());
+
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB 연결 성공"))
   .catch((err) => console.error("MongoDB 연결 실패:", err.message));
 
 
 app.get("/", (_req, res) => res.send("PhotoMemo API OK"));
+const authRoutes=require("./routes/authroutes")
+app.use("/api/auth",authRoutes)
 
 
 app.use((req, res) => {
 
   res.status(500).json({ message: "서버 오류"});
 });
+
 
 app.listen(PORT, () => {
   console.log(`Server running: http://localhost:${PORT}`);
